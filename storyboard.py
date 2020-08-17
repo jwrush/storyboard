0ebe011a56f9e0ea59f3b2b0a6fd64e671cad0d7
Rush <jwrush@gmail.com>
2020-08-17 11:30:00 -0500

    working on first script in earnest

--- storyboard.py
+++ storyboard.py
@@ -1,4 +1,5 @@
 import attr
+import json
 
 @attr.s(frozen=True) #frozen means immutable
 class Voice(object):
@@ -9,14 +10,16 @@ class Scene(object):
    description = attr.ib()
    panels = attr.ib()
 
+   def toJson(self):
+      dict = attr.asdict(self)
+      return json.dumps(dict)
+
 @attr.s(frozen=True)
 class Panel(object):
    description = attr.ib()
    bubbles = attr.ib()
 
-class Bubble:
-   def __init__(self, voice: Voice, contents: str):
-      self.contents = contents
-      self.voice = voice
-   def __str__(self):
-      return f"{str.upper(self.voice.name)}:\r\n{self.contents}\r\n"
+@attr.s(frozen=True)
+class Bubble(object):
+   voice = attr.ib()
+   contents = attr.ib()
--- test/storyboard_test.py
+++ test/storyboard_test.py
@@ -19,5 +19,24 @@ def test_VoiceMadeWithName():
 def test_BubbleMadeWithVoiceAndContents():
    voice = Voice("Bob")
    bubble = Bubble(voice, "Hello World!!")
-   bubble.voice is voice
+   assert bubble.voice is voice
    bubble.contents == "Hello World!!"
+
+def test_PanelMadeWithDescriptionAndBubbles():
+   voice = Voice("Bob")
+   bubble = Bubble(voice, "Hello World!!")
+   bubble2 = Bubble(voice, "Cats and dogs!!")
+   panel = Panel("Test", [bubble, bubble2])
+
+   assert panel.description == "Test"
+   assert panel.bubbles[0] is bubble
+ 
+def test_SceneSerialization():
+   voice = Voice("Bob")
+   bubble = Bubble(voice, "Hello World!!")
+   panel = Panel("Panel description", [bubble])
+   scene = Scene("Scene description", [panel])
+
+   json = scene.toJson()
+
+   assert json == '{"description": "Scene description", "panels": [{"description": "Panel description", "bubbles": [{"voice": {"name": "Bob"}, "contents": "Hello World!!"}]}]}'
